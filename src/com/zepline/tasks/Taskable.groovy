8365311830832322bdc34433b66687c6baa678e0
Purwandi <free6300@gmail.com>
2021-07-01 16:30:01 +0700

    Parsing

--- src/com/zepline/tasks/Taskable.groovy
+++ src/com/zepline/tasks/Taskable.groovy
@@ -4,9 +4,13 @@ abstract class Taskable {
   protected def script
   protected String name
 
-  def execute(def script, String name) {
+  def Taskable(def script, String name) {
     this.script = script
     this.name = name
+  }
+
+  def execute() {
+    return this.run()
   }
 
   abstract protected run()
