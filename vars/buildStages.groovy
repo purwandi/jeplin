b6285b88b8f2451017004c79b25ab42fa883e033
Purwandi <free6300@gmail.com>
2021-07-01 13:34:39 +0700

    Parsing

--- vars/buildStages.groovy
+++ vars/buildStages.groovy
@@ -3,16 +3,10 @@ import com.zepline.Zepline
 def call(Zepline zepline) {
   return { variables ->
     List<String> stagesA = zepline.stages
-    pipeline {
-      agent none
-      stages {
-        stagesA.each { stg ->
-          stage(stg) {
-            sh "echo ${stg}"
-          }
-        }
+    stagesA.each { stg ->
+      stage(stg) {
+        sh "echo ${stg}"
       }
     }
-    
   }
 }
