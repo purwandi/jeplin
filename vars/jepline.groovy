4bdf340e349bc4538eebbd030b0a782737449b4b
Purwandi <free6300@gmail.com>
2021-07-01 11:02:47 +0700

    Read yaml

--- vars/jepline.groovy
+++ vars/jepline.groovy
@@ -1,5 +1,8 @@
+// import groovy.yaml.YamlSlurper
+
 def call(String filename) {
-   def yaml = readYaml file: filename;
+  def config = readYaml file: filename
+  
   // def config = new YamlSlurper().parse(new File(filename))
   println(config)
 }
