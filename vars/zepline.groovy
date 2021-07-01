7023d97ec047de8978974a4f12af28062da3cbd1
Purwandi <free6300@gmail.com>
2021-07-01 12:27:26 +0700

    Parsing

---
+++ vars/zepline.groovy
@@ -0,0 +1,23 @@
+@Library('zepline')
+
+import com.zepline.parser.Config
+import com.zepline.*
+
+def call(String filename) {
+  def config = readYaml file: filename
+
+  Zepline zepline = Config.parse(config, env)
+
+  def closure = buildStages(zepline)
+
+  try {
+    closure([:])
+  } finally {
+    log.info("ok")
+  }
+  
+  // def config = new YamlSlurper().parse(new File(filename))
+  println(config)
+}
+
+// call(".jepline.yaml")
