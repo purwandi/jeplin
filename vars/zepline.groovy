54915a69369aa65bf680fd48142da53c64f303a3
Purwandi <free6300@gmail.com>
2021-07-01 17:57:48 +0700

    Parsing

--- vars/zepline.groovy
+++ vars/zepline.groovy
@@ -2,12 +2,8 @@ import com.zepline.Zepline
 
 def call(String filename) {
   def config = readYaml file: filename
-  // Zepline zepline = Config.parse(config, env, this)
   Zepline zepline = new Zepline(config).init()
-
-  // println(zepline)
-  // println(this)
-  // def closure = buildStages(zepline)
+  def closure = buildStages(zepline)
 
   try {
     closure([:])
