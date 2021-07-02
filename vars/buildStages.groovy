d211120ebaef25abf3256d74c67f243851f0e972
Purwandi <free6300@gmail.com>
2021-07-02 07:52:53 +0700

    Parsing

--- vars/buildStages.groovy
+++ vars/buildStages.groovy
@@ -3,8 +3,8 @@ import com.zepline.Zepline
 def call(Zepline zepline) {
   return { variables ->
     zepline.tasks.each { task ->
-      stage("cmd") {
-        sh "echo 'Hello'"
+      stage(task.name) {
+        sh "echo ${task.name}"
       }
     }
   }
