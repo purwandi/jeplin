b614c2aa7deab67980021351a00562afb8d0dba4
Purwandi <free6300@gmail.com>
2021-07-02 11:16:08 +0700

    Add docker and ssh task

--- src/com/zepline/tasks/TaskDocker.groovy
+++ src/com/zepline/tasks/TaskDocker.groovy
@@ -33,9 +33,9 @@ class TaskDocker extends Taskable {
 
   def run() {
     switch(input.command) {
-      case 'build': return this.build()
-      case 'push': return this.push()
-      case 'delete': return this.delete()
+      case 'build': return build()
+      case 'push': return push()
+      case 'delete': return delete()
       default:
         throw new Exception("Invalid input.command ${input.command} was not recognized")
       break
@@ -43,7 +43,8 @@ class TaskDocker extends Taskable {
   }
 
   def build() {
-    script.docker.WithRegistry(input.registry, input.credential) {
+    println(input)
+    script.docker.withRegistry(input.registry, input.credential) {
       script.docker.build(
         "${input.registry}/${input.repository}:${input.tag}",
         "-f ${input.dockerfile}",
@@ -53,7 +54,7 @@ class TaskDocker extends Taskable {
   }
 
   def push() {
-    script.docker.WithRegistry(input.registry, input.credential) {
+    script.docker.withRegistry(input.registry, input.credential) {
       script.docker.push("${input.registry}/${input.repository}:${input.tag}")
     }
   }
