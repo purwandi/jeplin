f8f9b2bf9631883072053c7c7d849d40bc258e60
Purwandi <free6300@gmail.com>
2021-07-03 21:50:36 +0700

    Refactor

--- src/com/zepline/Credentials.groovy
+++ src/com/zepline/Credentials.groovy
@@ -3,23 +3,23 @@
 *    credentialId - credential id
 */
 
-def credentials = CredentialsProvider.lookupCredentials(
-    StandardUsernamePasswordCredentials.class, 
-    Jenkins.instance
-);
+// def credentials = CredentialsProvider.lookupCredentials(
+//     StandardUsernamePasswordCredentials.class, 
+//     Jenkins.instance
+// );
           
-def cred = credentials.findResult { it.id == credentialId ? it : null }
-def username = cred.username;
-def password = cred.password;
+// def cred = credentials.findResult { it.id == credentialId ? it : null }
+// def username = cred.username;
+// def password = cred.password;
 
 
-def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-        com.cloudbees.plugins.credentials.Credentials.class,
-        Jenkins.instance,
-        null,
-        null
-);
+// def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+//         com.cloudbees.plugins.credentials.Credentials.class,
+//         Jenkins.instance,
+//         null,
+//         null
+// );
 
-for (c in creds) {
-  println(c.id + ": " + c.description)
-}
+// for (c in creds) {
+//   println(c.id + ": " + c.description)
+// }
