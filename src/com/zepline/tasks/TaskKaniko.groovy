07588842c6bdaa19dc340935b3fcccdddfcf49c2
Purwandi <free6300@gmail.com>
2021-07-03 12:34:33 +0700

    Add kaniko

---
+++ src/com/zepline/tasks/TaskKaniko.groovy
@@ -0,0 +1,24 @@
+package com.zepline.tasks
+
+class TaskKaniko extends Taskable {
+  TaskDockerInput input
+
+  TaskKaniko(def yaml, def script) {
+    super("kaniko", yaml, script)
+
+    this.input = new TaskDockerInput(yaml.input, script)
+  }
+
+  def run() {
+    script.docker.withRegistry(input.registry, input.credential) {
+      script.sh "/kaniko/executor
+        --insecure --skip-tls-verify --insecure-pull --skip-tls-verify-pull 
+        --insecure-registry --skip-tls-verify-registry
+        --dockerfile ${input.dockerfile}
+        --context ${input.context}
+        ${input.args}
+        --destination ${input.repository}:${input.tag}
+      "
+    }
+  }
+}
