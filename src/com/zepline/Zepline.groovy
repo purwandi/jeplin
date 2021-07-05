556aba9f942e7cec7876537438e27ab34725cd27
Purwandi <free6300@gmail.com>
2021-07-05 09:33:03 +0700

    add some labs

--- src/com/zepline/Zepline.groovy
+++ src/com/zepline/Zepline.groovy
@@ -16,7 +16,29 @@ class Zepline {
     this.yaml.tasks.each { k, v ->
       def config = new Config(v, yaml)
       this.tasks[k] = new Task(k, config, script)
-    //   return new Task(k, new Config(v, yaml), script)
+    }
+
+    if (stages) {
+      // check if all task have stage property
+      tasks.each { k, v -> 
+        if (v.stage == null) {
+          throw new Exception("The stage property in '${k}' is not defined ")
+        }
+
+        if (stages.contains(v.stage) == false) {
+          throw new Exception("The stage property in '${k}' is not defined in stages ")
+        }
+      }
+
+      // sort tasks by stage
+      def closure = [:]
+      def groups = tasks.groupBy(it.value.stage)
+      stages.collect{k ->
+        if (groups[k] != null) {
+          closure[k] = groups[k]
+        }
+      }
+      this.tasks = closure
     }
 
     // this.tasks = config.tasks.collect { k, item ->
