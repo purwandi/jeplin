258a0afe456e5129e2fae59457b70b7e13a8826e
Purwandi <free6300@gmail.com>
2021-07-05 07:39:49 +0700

    add some labs

--- vars/zepline.groovy
+++ vars/zepline.groovy
@@ -1,15 +1,12 @@
 import com.zepline.Zepline
 
 def call(String filename) {
-  def config = readYaml file: filename
+  def config = readYaml file: ".zepline.yaml"
   if (config == null) {
     return
   }
 
-  sh "rm -rf $WORKSPACE/.config/.zepline.yaml || true"
-  dir(".config") {
-    sh "cat $WORKSPACE/.zepline.yaml >> $WORKSPACE/.config/.zepline.yaml"
-  }
+  def files = readFile ".zepline.yaml"
   
   // clone include project
   if (config.include) {
@@ -17,16 +14,9 @@ def call(String filename) {
       dir('.include') {
         git branch: item.ref , credentialsId: item.credential, url: item.remote 
       }
-
-      // sh "echo print item ${item}"
-      item.files.each { f -> 
-        sh "echo ${f}"
-        sh "cat $WORKSPACE/.include${f}" 
-        sh "cat $WORKSPACE/.include${f} >> $WORKSPACE/.config/.zepline.yaml" 
-      }
     }
   }
 
+  println files
   sh 'ls -all'
-  sh "cat $WORKSPACE/.config/.zepline.yaml"
 }
