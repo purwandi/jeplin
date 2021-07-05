822aa255fc6f31e58abd4a6808c2979d3f433081
Purwandi <free6300@gmail.com>
2021-07-05 14:43:01 +0700

    add some labs

---
+++ src/com/zepline/WithCredentials.groovy
@@ -0,0 +1,74 @@
+package com.zepline
+
+class WithCredentials {
+
+  def creds = []
+
+  static def parse(def yaml, def script) {
+    def self = new WithCredentials()
+
+    yaml.each { item ->
+      // switch(item.type) {
+      //   case "usernamePassword": 
+      //     return self.passUsernamePassword(item, script)
+      //   case "usernameColonPassword": 
+      //     return self.passUsernameColonPassword(item, script)
+      //   case "file": 
+      //     return self.passFile(item, script)
+      //   case "string": 
+      //     return self.passString(item, script)
+      //   default:
+      //     throw new Exception("Undefined '${item.type}' credential ")
+      //     break
+      // }
+    }
+
+    return this
+
+    // return script.withCredentials(credentials) {
+    //   closure()
+    // }
+  }
+
+  def execute(def task) {
+    return task()
+  }
+
+  def passUsernamePassword(def item, def script) {
+    this.creds.add(script.usernamePassword(
+      credentialsId: item.credential,
+      usernameVariable: item.variables.username,
+      passwordVariable: item.variables.password
+    ))
+
+    return this
+  }
+
+  def passUsernameColonPassword(def item, def script) {
+    this.creds.add(script.usernameColonPassword(
+      credentialsId: item.credential,
+      variable: item.variables.variable
+    ))
+
+    return this
+  }
+
+  def passFile(def item, def script) {
+    this.creds.add(script.file(
+      credentialsId: item.credential,
+      variable: item.variables.variable
+    ))
+
+    return this
+  }
+
+  def passString(def item, def script) {
+    this.creds.add(script.string(
+      credentialsId: item.credential,
+      variable: item.variables.variable
+    ))
+
+    return this
+  }
+  
+}
