c6b67c74711f90f6a1c5e037b4bc68f183498823
Purwandi <free6300@gmail.com>
2021-07-07 22:23:21 +0700

    work image registry

--- src/com/zepline/WithImageRegistry.groovy
+++ src/com/zepline/WithImageRegistry.groovy
@@ -3,17 +3,35 @@ package com.zepline
 class WithImageRegistry {
 
   static def parse(def config, def cfgRegistry, def script, def closure) {
+    def getHost = { url ->
+        String domainName = new String(url)
+        int index = domainName.indexOf("://")
+        if (index != -1) {
+          // keep everything after the "://"
+          domainName = domainName.substring(index + 3)
+        }
+
+        index = domainName.indexOf('/')
+
+        if (index != -1) {
+          // keep everything before the '/'
+          domainName = domainName.substring(0, index)
+        }
+
+        return domainName.replaceFirst("^www.*?\\.", "")
+      }
+    }
+
     // if config registy is null 
     if (cfgRegistry == null) {
-      return {
-        closure()
-      }
+      closure()
+      return 
     }
 
     // loop in available docker property credentials
     def registry
     cfgRegistry.each { cfg -> 
-      def hostname = new URL(cfg.registry).getHost()
+      def hostname = getHost(cfg.registry)
       if (config.image.contains(hostname)) {
         registry = cfg
       }
@@ -21,17 +39,15 @@ class WithImageRegistry {
     
     // if image is not match in existing docker property we need to return it
     if (registry == null) {
-      return {
-        closure()
-      }
+      closure()
+      return
     }
 
     // wrap closure using docker registry
-    return {
-      script.withDockerRegistry([url: registry.registry, credentialsId: registry.credential]) {
+    return script
+      .withDockerRegistry([url: registry.registry, credentialsId: registry.credential]) {
         closure()
       }
-    }
   }
 
 }
