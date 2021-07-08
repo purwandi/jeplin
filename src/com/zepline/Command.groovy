65930b81c3d9ff7a7ae81523ce0c5bd44e8416a7
Purwandi <free6300@gmail.com>
2021-07-08 21:43:32 +0700

    Debug

--- src/com/zepline/Command.groovy
+++ src/com/zepline/Command.groovy
@@ -3,9 +3,9 @@ package com.zepline
 class Command {
   static Command parse(def jenkins, def command) {
     if (jenkins.isUnix()) {
-      jenkins.sh (script: "$command")
+      jenkins.sh command
     } else {
-      jenkins.bat (script: "$command")
+      jenkins.powershell command
     }
   }
 }
