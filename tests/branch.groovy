ef9221447d295134bd37441299c4b0714a0611b1
Purwandi <free6300@gmail.com>
2021-07-08 18:40:22 +0700

    remove test folder

--- tests/branch.groovy
+++
@@ -1,9 +0,0 @@
-def only = ["develop", "master", "tags"]
-def master = "remotes/origin/develop"
-def tags = "tags/v1.0.0"
-
-println tags.replace("remotes/origin/", "")
-println tags.contains("tags")
-
-println only.contains("test")
-
