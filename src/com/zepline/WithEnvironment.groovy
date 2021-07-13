2499939c730a9feffe329dc6a3a54ab519c85f53
Purwandi <free6300@gmail.com>
2021-07-13 13:18:08 +0700

    Debug

--- src/com/zepline/WithEnvironment.groovy
+++ src/com/zepline/WithEnvironment.groovy
@@ -2,8 +2,9 @@ package com.zepline
 
 class WithEnvironment {
   static def parse(def config, def script, def closure) {
+    script.sh "echo $config"
     return {
-      script.withEnv(config.variables) {
+      script.withEnv(config) {
         closure()
       }
     }
