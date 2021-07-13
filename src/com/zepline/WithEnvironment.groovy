a89387b10396f3cc446f8a4eb3b582562ce84814
Purwandi <free6300@gmail.com>
2021-07-13 12:33:12 +0700

    Debug

--- src/com/zepline/WithEnvironment.groovy
+++ src/com/zepline/WithEnvironment.groovy
@@ -8,7 +8,8 @@ class WithEnvironment {
 
     config.variables.each { val -> 
       if (jenkins.isUnix()) {
-        jenkins.env."${val.key}" = jenkins.sh(script: "echo ${val.value}", returnStdout: true).trim()
+        jenkins.env."${val.key}" = "$(echo ${val.value})"
+        // jenkins.env."${val.key}" = jenkins.sh(script: "echo ${val.value}", returnStdout: true).trim()
       } else {
         jenkins.env."${val.key}" = jenkins.powershell(script: "echo ${val.value}", returnStdout: true).trim()
       }
