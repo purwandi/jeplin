329a24236649302997be655f0e72d82be5b4e380
Purwandi <free6300@gmail.com>
2021-07-13 14:29:04 +0700

    Debug

--- src/com/zepline/WithEnvironment.groovy
+++ src/com/zepline/WithEnvironment.groovy
@@ -2,9 +2,14 @@ package com.zepline
 
 class WithEnvironment {
   static def parse(def config, def jenkins) {
+    jenkins.sh "echo $config"
     jenkins.script {
       config.each { k, v -> 
-        jenkins.env."$k" = "$v"
+        if (jenkins.isUnix()) {
+          jenkins.env."$k" = jenkins.sh(script: "echo $v", returnStdout: true).trim()
+        } else {
+          jenkins.env."$k" = jenkins.powershell(script: "echo $v", returnStdout: true).trim()
+        }
       }
     }
   }
