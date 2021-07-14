9664a305aeaf168886272276832aeb230dde5ed9
Purwandi <free6300@gmail.com>
2021-07-14 16:16:52 +0700

    Add global varibles

--- src/com/zepline/Config.groovy
+++ src/com/zepline/Config.groovy
@@ -8,7 +8,7 @@ class Config {
   List<Service> services      = []
   def           variables     = [:]
   def           credentials   = []
-  def           docker
+  def           docker        = []
   List<String>  when
   List<String>  only
   def  before_script
@@ -25,9 +25,14 @@ class Config {
   }
 
   def parse() {
+    // Parse global
+    parseVarConfig(yaml.".variables")
+    parseCredentialConfig(yaml.".credentials")
+    parseDockerCredentialConfig(yaml.".docker")
+
     // Parse variables
-    parseVarConfig(yaml)
-    parseVarConfig(config)
+    parseVarConfig(yaml.variables)
+    parseVarConfig(config.variables)
     if (config.extend) {
       def cfgExtends = yaml."${config.extend}"
       if (cfgExtends != null) {
@@ -36,23 +41,52 @@ class Config {
     }
     
     // parse global
-    parseConfig(yaml, true)
-    parseConfig(config, true)
+    parseConfig(yaml)
+    parseConfig(config)
 
     return this
   }
 
   def parseVarConfig(def cfg) {
-    if (cfg.variables == null) {
+    if (cfg == null) {
       return
     }
 
-    cfg.variables.each { k, v -> 
+    cfg.each { k, v -> 
       this.variables[k] = v
     }
   }
 
-  def parseConfig(def cfg, def highOrder) {
+  def parseCredentialConfig(def cfg) {
+    if (cfg == null) {
+      return
+    }
+
+    cfg.each { c -> 
+      def idx = this.credentials.findIndexOf{it.credential == c.credential}
+      if (idx >= 0) {
+        this.credentials[idx] = c
+      } else {
+        this.credentials.add(c)
+      }
+    }
+  }
+
+  def parseDockerCredentialConfig(def cfg) {
+    if (cfg == null) {
+      return
+    }
+    cfg.each { c -> 
+      def idx = this.docker.findIndexOf{it.registry == c.registry}
+      if (idx >= 0) {
+        this.docker[idx] = c
+      } else {
+        this.docker.add(c)
+      }
+    }
+  }
+
+  def parseConfig(def cfg) {
     cfg.each { key, val -> 
       if (!["name","image","stage","services","credentials","docker","when","only","before_script","script","after_script","extend"].contains(key)) {
         return
@@ -62,19 +96,12 @@ class Config {
         if (key == "extend") {
           def cfgExtends = yaml."$val"
           if (cfgExtends != null) {
-            parseConfig(cfgExtends, false)
+            parseConfig(cfgExtends)
           }
-        } else if (key == "variables") {
-          return
         } else if (key == "credentials") {
-          val.each { c -> 
-            def idx = this.credentials.findIndexOf{it.credential == c.credential}
-            if (idx >= 0) {
-              this.credentials[idx] = c
-            } else {
-              this.credentials.add(c)
-            }
-          }
+          parseCredentialConfig(val)
+        } else if (key == "docker") {
+          parseDockerCredentialConfig(val)
         } else if (key == "image") {
           this."$key" = new Image(val)
         } else if (key == "services") {
